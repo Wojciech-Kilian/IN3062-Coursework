8a57c798e25abf7a85fc949b68b1da7204aba3f2
Wojciech-Kilian <48245561+Wojciech-Kilian@users.noreply.github.com>
2021-08-23 17:48:37 +0100

    Update Model 1.py
    Update on first model with R^2

--- Model 1.py
+++ Model 1.py
@@ -9,42 +9,51 @@ import pandas as pd
 import numpy as np
 import scipy as sp
 import matplotlib.pyplot as plt  
+import sklearn as sk
 
+#Excel file should be in same directory
 path = "."
-
 filename_read = os.path.join(path, "Life Expectancy Data.csv")
+#check for empty fields
 df = pd.read_csv(filename_read, na_values=['NA', '?'])
 
-# First data frame
+#Dropping of Human Development Index in terms of income composition of resources column
+#I found out that his HDI value is " is assessed by life expectancy at birth(...)"
+# as well as schooling value and GNI which from my understanding is correlated with GDP
+#it has been deleted to prevent bias in results
+df.drop('Income composition of resources', 1, inplace=True)
+
+#print(df1.isnull().any())
+
+# First data frame - dropping non numerical features
 df1 = df.select_dtypes(include=['int', 'float'])
 
 headers = list(df1.columns.values)
 fields = []
      
-#repeating with dropping values
+#Preparing data by dropping empty values instead of filling with median
+df1 = df1.dropna()
+
+#Calculating Pearson's correlation coefficient for each feature
 for field in headers:
-    df1 = df1.dropna()
     fields.append({
         'name' : field,
-        #'mean': df[field].mean(),
-        #'var': df[field].var(),
-        #'sdev': df[field].std(),
         'Pearson`s corr coef': sp.stats.pearsonr(df1['Life expectancy '],df1[field])
     })
-#print(df['Life expectancy '])    
+    
 for field in fields:
     print(field)
     
-#dropping values gives much more promising results in terms of probability p of correlation
+#dropping values gives (in my opinion) much more promising results in terms of probability p of correlation
+    
 #can we predict life expectancy of a country by given data?
     
-#Creating regression model for life expectancy
+#Creating regression model for life expectancy predicition
 from sklearn.model_selection import train_test_split 
 from sklearn.linear_model import LinearRegression
 from sklearn import metrics
 
-#print(df1.isnull().any())
-
+#Collecting columns other than target one
 result = []
 for x in df1.columns:
     if x != 'Life expectancy ':
@@ -52,28 +61,55 @@ for x in df1.columns:
    
 X = df1[result].values
 y = df1['Life expectancy '].values
-#for i in range(50):
-    #, random_state=i*10)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.125, random_state=50)
 
+#instead of using K-fold I found it much easier to change random states to get different datasplits
+#the best score (RSME percent of mean has been found on model with Random state =22040)
+#the best score (R^2 has been found on model with Random state =16720)
+#best=0
+#best2=0
+#best_ratio=100
+#best_R2=0
+#for i in range(5000):
+#, random_state=i*10)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.125, random_state=16720)
+    
 # build the model
 model = LinearRegression()  
 model.fit(X_train, y_train)
-
+    
 print(model.coef_)
-
+    
 y_pred = model.predict(X_test)
-
+    
 #build a new data frame with two columns, the actual values of the test data, 
 #and the predictions of the model
-
+    
 df1_compare = pd.DataFrame({'Actual': y_test, 'Predicted': y_pred})
-df1_head = df1_compare.head(7)
-print(df1_head)
+#print first 10
+df1_head = df1_compare.head(10)
+#print(df1_head)
+#Calculate Mean, RMSE and how many percent of mean is our RMSE.
+#According to lecture information percent below 10% would be satisfying
+#This produced model has ratio of 0.04408479536999818 ~ slightly below 4.5%
+#I also calculate R^2 - coefficient of determination
+#the best R^2 was 0.870799188733013 from state 16720
+    
 print('Mean:', np.mean(y_test))
 print('RMSE:', np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
-print("RSME percent of mean:",np.sqrt(metrics.mean_squared_error(y_test, y_pred))/np.mean(y_test))
-
+rpom=np.sqrt(metrics.mean_squared_error(y_test, y_pred))/np.mean(y_test)
+print("RSME percent of mean:",rpom)
+r2=sk.metrics.r2_score(y_test, y_pred)
+print("R^2",r2)
+    #if rpom < best_ratio:
+    #    best_ratio=rpom
+    #    best=i
+    #if r2> best_R2:
+    #    best_R2=r2
+    #    best2=i
+    #print(best)
+    #print(best_ratio)
+    #print(best2)
+    #print(best_R2)
 def chart_regression(pred, y, sort=True):
     t = pd.DataFrame({'pred': pred, 'y': y.flatten()})
     if sort:
@@ -83,26 +119,25 @@ def chart_regression(pred, y, sort=True):
     plt.ylabel('output')
     plt.legend()
     plt.show()
+    
 chart_regression(y_pred[:50].flatten(),y_test[:50],sort=True)
 chart_regression(y_pred[:100].flatten(),y_test[:100],sort=True)
 chart_regression(y_pred[:200].flatten(),y_test[:200],sort=True)   
 
-#second data frame
-#Strip non-numeric data
+#Second data frame - dropping non numerical features
+
 #df2 = df.select_dtypes(include=['int', 'float'])
+
 #headers = list(df2.columns.values)
 #fields = []
 
-#using median value to fill NA fields 
+##Preparing data by filling empty values with median instead of dropping
 #for field in headers:
 #    med = df2[field].median()
 #    df2[field] = df2[field].fillna(med)
-#    print(f" #{field} has na? {pd.isnull(df[field]).values.any()}")
+#    print(f" #{field} has na? {pd.isnull(df2[field]).values.any()}")
 #    fields.append({
 #        'name' : field,
-#        #'mean': df2[field].mean(),
-#        #'var': df2[field].var(),
-#        #'sdev': df2[field].std(),
 #        'Pearson`s corr coef': sp.stats.pearsonr(df2[field], df2['Life expectancy '])
 #    }) 
 #for field in fields:
