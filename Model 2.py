4f42f7ecca608e07b3bf5d91749bb71f9f385c9b
Wojciech-Kilian <48245561+Wojciech-Kilian@users.noreply.github.com>
2021-08-24 04:22:40 +0100

    Saved network

--- Model 2.py
+++ Model 2.py
@@ -50,7 +50,10 @@ filename = os.path.join(path,"Life Expectancy Data.csv")
 df = pd.read_csv(filename,na_values=['NA','?'])
 
 df.drop('Income composition of resources', 1, inplace=True)
+df.drop('Population', 1, inplace=True)
+df.drop('Total expenditure', 1, inplace=True)
 df.drop('Country', 1, inplace=True)
+
 encode_text_index(df,'Status')
 
 headers = list(df.columns.values)
@@ -66,15 +69,17 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.125, rando
 
 model = Sequential()
 model.add(Dense(40, input_dim=X.shape[1], activation='sigmoid',kernel_regularizer=regularizers.l2(0.01))) # Hidden 1
-model.add(Dropout(0.05))
-model.add(Dense(40, activation='sigmoid'))
-model.add(Dense(40, activation='relu')) # Hidden 2
+model.add(Dropout(0.05)) #Hidden 2
+model.add(Dense(40, activation='sigmoid')) #Hidden 3
+model.add(Dense(40, activation='relu')) # Hidden 4
 model.add(Dense(1)) # Output
 model.compile(loss='mean_squared_error', optimizer='adam')
 monitor = EarlyStopping(monitor='loss', patience=5,min_delta=0.01, mode='min')
-model.fit(X_train,y_train,verbose=2,epochs=200,callbacks=[monitor])
+model.fit(X_train,y_train,verbose=2,epochs=100,callbacks=[monitor])
 
 model.summary()
+
+#model.save(os.path.join(".","network.h5"))
 
 pred = model.predict(X_test)
 print("Shape: {}".format(pred.shape))
